4064336ba6ed6a2ea13cc7cb738199691bae883c
Anna <annatos3006@mail.ru>
2026-02-04 20:55:08 +0300

    Fix failing tests

--- src/test/java/com/example/PostmanEchoTest.java
+++ src/test/java/com/example/PostmanEchoTest.java
@@ -1,14 +1,13 @@
 package com.example;
 
 import org.junit.jupiter.api.Test;
-
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
 public class PostmanEchoTest {
 
     @Test
-    public void testPostRequest() {
+    void testPostRequestWithSimpleData() {
         given()
                 .baseUri("https://postman-echo.com")
                 .body("some data")
@@ -16,6 +15,45 @@ public class PostmanEchoTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("wrong data")); // изменено на неверное значение
+                .body("data", equalTo("some data");
+    }
+
+    @Test
+    void testPostRequestWithRussianText() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("Привет, мир!")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Привет, мир!"));
+    }
+
+    @Test
+    void testPostRequestWithJsonData() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("application/json")
+                .body("{\"test\": \"value\"}")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("json.test", equalTo("value"));
+    }
+
+    // Тест, который должен упасть - демонстрация fail в CI
+    @Test
+    void testFailingPostRequest() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("correct data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("wrong data")); // Намеренно неверное утверждение
     }
 }
